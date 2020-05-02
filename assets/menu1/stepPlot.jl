bab5335bb6e30d91d2121e75e20d09e97ca67dfb
lazarusA <lazarus.alon@gmail.com>
2020-05-02 13:32:04 +0000

    Deploying to master from  @ 32a83bed6cb00704a58bfee4fb515368c9feb2e3 🚀

---
+++ assets/menu1/stepPlot.jl
@@ -0,0 +1,6 @@
+using Gnuplot, Random
+x = randn(1000)
+h = hist(x, bs = 0.1)
+c = "#1E90FF"
+@gp h.bins h.counts "with steps tit '' lc '$(c)' lw 2" "set grid"
+save(term="pngcairo size 600,400", output = "stepPlot.png")
